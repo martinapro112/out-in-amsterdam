1240dbcc43884da99698f0d2b7c309453b84e98d
Martina Prochazkova <martinapro112@gmail.com>
2019-10-21 22:41:31 +0200

    [Added] title element in main page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 import Venues from './Venues/List';
+import { Jumbotron, Container } from 'react-bootstrap';
 
 class App extends Component {
     render() {
         return (
             <div id="app">
+                <Jumbotron fluid>
+                    <Container>
+                        <h1>Out in Amsterdam</h1>
+                        <p>
+                            Let's find you the perfect venue.
+                        </p>
+                    </Container>
+                </Jumbotron>
                 <Venues />
             </div>
         );
