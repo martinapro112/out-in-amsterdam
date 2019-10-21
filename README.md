d719da5d719512b8889de235e32ee4d8ff01d2c6
Martina Prochazkova <martinapro112@gmail.com>
2019-10-21 20:55:32 +0200

    [Added] blank project

---
+++ README.md
@@ -0,0 +1,6 @@
+This is a demonstrative project containing filterable list of various venues - restaurants, cafes and
+bars - in and around the city of Amsterdam.
+
+- clone or copy repository
+- run 'npm i'
+- run 'npm start'
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    render() {
+        return (
+            <div id="app">
+                blank
+            </div>
+        );
+    }
+}
+
+export default App;
